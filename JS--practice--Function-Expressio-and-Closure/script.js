20081f30c431a9235c8af9119870d64f95a1263f
aleksandra05sh <aleksandra05sh@gmal.com>
2021-09-09 15:32:14 +0300

    last task redone

--- JS--practice--Function-Expressio-and-Closure/script.js
+++ JS--practice--Function-Expressio-and-Closure/script.js
@@ -49,58 +49,117 @@ var add = function (x, y) {
 Сторонами прямоугольника может быть только число (тип данных Number, значения других типов данных нельзя устанавливать), значение которого, строго больше 0.*/
 
 
+// function createSafePerimeterSquare(){
+//     var arr = [];
+//     var sideA;
+//     var sideB;
+//     var error;
+
+//     arr[0] = function (number1, number2) {
+//         if (typeof number1 === "number" && typeof number2 === "number" && number1 > 0 && number2 > 0) {
+//             sideA = number1;
+//             sideB = number2;
+//             error = false;
+
+//         } else {
+//             error = true;
+//         }
+//     }   
+
+//     arr[1] = function() {
+//         var Perimeter = 2 * (sideA + sideB);
+//         var Square = sideA * sideB;
+        
+//         if (error) {
+//             return "Ведены некорректные данные";
+//         } else {
+//             return ["Периметр равен: " + Perimeter, "Площадь равна: " + Square];
+//         }
+//     }
+
+//     return arr;
+// }    
+
+
+// var PerimeterSquare = createSafePerimeterSquare();
+// PerimeterSquare[0](1, 0);
+// console.log(PerimeterSquare[1]());
+
+// PerimeterSquare[0](1, 2);
+// console.log(PerimeterSquare[1]());
+
+// PerimeterSquare[0](-1, "привет");
+// console.log(PerimeterSquare[1]());
+
+// PerimeterSquare[0](true, 2);
+// console.log(PerimeterSquare[1]());
+
+// PerimeterSquare[0](NaN, 2);
+// console.log(PerimeterSquare[1]());
+
+// PerimeterSquare[0]("", 2);
+// console.log(PerimeterSquare[1]());
+
+// PerimeterSquare[0](null, 2);
+// console.log(PerimeterSquare[1]());
+
+
+
+
+/*Сюда ) Решение с правками */
+
 function createSafePerimeterSquare(){
     var arr = [];
     var sideA;
     var sideB;
-    var error;
 
-    arr[0] = function (number1, number2) {
-        if (typeof number1 === "number" && typeof number2 === "number" && number1 > 0 && number2 > 0) {
+    arr[0] = function (number1) {
+        if (typeof number1 === "number" && number1 > 0) {
             sideA = number1;
-            sideB = number2;
-            error = false;
-
         } else {
-            error = true;
-        }
-    }   
-
-    arr[1] = function() {
-        var Perimeter = 2 * (sideA + sideB);
-        var Square = sideA * sideB;
-        
-        if (error) {
-            return "Ведены некорректные данные";
-        } else {
-            return ["Периметр равен: " + Perimeter, "Площадь равна: " + Square];
+            sideA = 0;
+            console.log("Введено некорректное значение");
         }
     }
 
-    return arr;
-}    
+    arr[1] = function (number2) {
+        if (typeof number2 === "number" && number2 > 0) {
+            sideB = number2;
+        }
+        else {
+            sideB = 0;
+            console.log("Введено некорректное значение");
+        }
+    } 
 
+    arr[2] = function Perimeter() {
+        var Perimeter = 2 * (sideA + sideB);
+        if (sideA === 0 || sideB === 0 ) {
+            return "Были введены некорректные данные";
+            } else {
+            return "Периметр равен: " + Perimeter ;
+            }
+    }
+
+    arr[3] = function Square() {
+        var Square = sideA * sideB;
+        if (sideA === 0 || sideB ===0 ) {
+            return "Были введены некорректные данные";
+            } else {
+            return "Площадь равна:: " + Square;
+            }
+    }
+    return arr;
+
+}
 
 var PerimeterSquare = createSafePerimeterSquare();
-PerimeterSquare[0](1, 0);
-console.log(PerimeterSquare[1]());
+PerimeterSquare[0](5);
+PerimeterSquare[1](6);
+console.log(PerimeterSquare[2]());
+console.log(PerimeterSquare[3]());
 
-PerimeterSquare[0](1, 2);
-console.log(PerimeterSquare[1]());
-
-PerimeterSquare[0](-1, "привет");
-console.log(PerimeterSquare[1]());
-
-PerimeterSquare[0](true, 2);
-console.log(PerimeterSquare[1]());
-
-PerimeterSquare[0](NaN, 2);
-console.log(PerimeterSquare[1]());
-
-PerimeterSquare[0]("", 2);
-console.log(PerimeterSquare[1]());
-
-PerimeterSquare[0](null, 2);
-console.log(PerimeterSquare[1]());
-
-
+PerimeterSquare[0](-1);
+PerimeterSquare[1](6);
+console.log(PerimeterSquare[2]());
+console.log(PerimeterSquare[3]());
